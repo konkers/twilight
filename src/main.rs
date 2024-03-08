60b408ad46ded7872ae347825c1631d1419238df
Erik Gilling <konkers@konkers.net>
2024-03-07 19:12:12 -0800

    Initial BLE sample commit

---
+++ src/main.rs
@@ -0,0 +1,155 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+#![feature(async_closure)]
+
+use core::cell::RefCell;
+
+use bleps::{
+    ad_structure::{
+        create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE,
+    },
+    async_attribute_server::AttributeServer,
+    asynch::Ble,
+    attribute_server::NotificationData,
+    gatt,
+};
+use embassy_executor::Spawner;
+use embedded_hal_async::digital::Wait;
+use esp32c3_hal as hal;
+use esp_backtrace as _;
+use esp_println::println;
+use esp_wifi::{ble::controller::asynch::BleConnector, initialize, EspWifiInitFor};
+use hal::{clock::ClockControl, embassy, peripherals::*, prelude::*, timer::TimerGroup, Rng, IO};
+
+#[main]
+async fn main(_spawner: Spawner) -> ! {
+    #[cfg(feature = "log")]
+    esp_println::logger::init_logger(log::LevelFilter::Info);
+
+    let peripherals = Peripherals::take();
+
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::max(system.clock_control).freeze();
+
+    #[cfg(target_arch = "riscv32")]
+    let timer = hal::systimer::SystemTimer::new(peripherals.SYSTIMER).alarm0;
+    let init = initialize(
+        EspWifiInitFor::Ble,
+        timer,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let button = io.pins.gpio9.into_pull_down_input();
+
+    // Async requires the GPIO interrupt to wake futures
+    hal::interrupt::enable(
+        hal::peripherals::Interrupt::GPIO,
+        hal::interrupt::Priority::Priority1,
+    )
+    .unwrap();
+
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    embassy::init(&clocks, timer_group0);
+
+    let mut bluetooth = peripherals.BT;
+
+    let connector = BleConnector::new(&init, &mut bluetooth);
+    let mut ble = Ble::new(connector, esp_wifi::current_millis);
+    println!("Connector created");
+
+    let pin_ref = RefCell::new(button);
+    let pin_ref = &pin_ref;
+
+    loop {
+        println!("{:?}", ble.init().await);
+        println!("{:?}", ble.cmd_set_le_advertising_parameters().await);
+        println!(
+            "{:?}",
+            ble.cmd_set_le_advertising_data(
+                create_advertising_data(&[
+                    AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+                    AdStructure::ServiceUuids16(&[Uuid::Uuid16(0x1809)]),
+                    AdStructure::CompleteLocalName("konkers"),
+                ])
+                .unwrap()
+            )
+            .await
+        );
+        println!("{:?}", ble.cmd_set_le_advertise_enable(true).await);
+
+        println!("started advertising");
+
+        let mut rf = |_offset: usize, data: &mut [u8]| {
+            data[..20].copy_from_slice(&b"Hello Bare-Metal BLE"[..]);
+            17
+        };
+        let mut wf = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: {} {:?}", offset, data);
+        };
+
+        let mut wf2 = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: {} {:?}", offset, data);
+        };
+
+        let mut rf3 = |_offset: usize, data: &mut [u8]| {
+            data[..5].copy_from_slice(&b"Hola!"[..]);
+            5
+        };
+        let mut wf3 = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: Offset {}, data {:?}", offset, data);
+        };
+
+        gatt!([service {
+            uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
+            characteristics: [
+                characteristic {
+                    uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
+                    read: rf,
+                    write: wf,
+                },
+                characteristic {
+                    uuid: "957312e0-2354-11eb-9f10-fbc30a62cf38",
+                    write: wf2,
+                },
+                characteristic {
+                    name: "my_characteristic",
+                    uuid: "987312e0-2354-11eb-9f10-fbc30a62cf38",
+                    notify: true,
+                    read: rf3,
+                    write: wf3,
+                },
+            ],
+        },]);
+
+        let mut rng = bleps::no_rng::NoRng;
+        let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes, &mut rng);
+
+        let counter = RefCell::new(0u8);
+        let counter = &counter;
+
+        let mut notifier = || {
+            // TODO how to check if notifications are enabled for the characteristic?
+            // maybe pass something into the closure which just can query the characterisic value
+            // probably passing in the attribute server won't work?
+            async {
+                #[allow(clippy::await_holding_refcell_ref)]
+                pin_ref.borrow_mut().wait_for_rising_edge().await.unwrap();
+                let mut data = [0u8; 13];
+                data.copy_from_slice(b"Notification0");
+                {
+                    let mut counter = counter.borrow_mut();
+                    data[data.len() - 1] += *counter;
+                    *counter = (*counter + 1) % 10;
+                }
+                NotificationData::new(my_characteristic_handle, &data)
+            }
+        };
+
+        srv.run(&mut notifier).await.unwrap();
+    }
+}
