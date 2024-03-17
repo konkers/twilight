cf89adbf6b07363297c7016950b4ac1e78fc9e43
Erik Gilling <konkers@konkers.net>
2024-03-17 13:42:12 -0700

    Clean up warnings

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,14 @@
 #![feature(type_alias_impl_trait)]
 #![feature(async_closure)]
 
-use de1::{fake::De1, Command, Frame};
+use de1::Frame;
 use embassy_executor::Spawner;
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_sync::channel::{Channel, Receiver, Sender};
-use embassy_sync::pipe::Pipe;
 use esp32c3_hal as hal;
 use esp_backtrace as _;
 use esp_wifi::{initialize, EspWifiInitFor};
-use hal::uart::{AllPins, TxRxPins};
-use hal::{clock::ClockControl, embassy, peripherals::*, prelude::*, timer::TimerGroup, Rng, IO};
-use hal::{uart, Uart};
+use hal::{clock::ClockControl, embassy, peripherals::*, prelude::*, timer::TimerGroup, Rng};
 use log::info;
 
 mod ble;
@@ -25,15 +22,6 @@ use ble::ble_task;
 type FrameChannel = Channel<NoopRawMutex, Frame, 4>;
 type FrameSender<'ch> = Sender<'ch, NoopRawMutex, Frame, 4>;
 type FrameReceiver<'ch> = Receiver<'ch, NoopRawMutex, Frame, 4>;
-
-// enum Subscribtion {
-//     Subscribe(Command),
-//     Unsubscribe(Command),
-// }
-
-// type SubscribtionChannel = Channel<NoopRawMutex, Subscribtion, 4>;
-// type SubscribtionSender<'ch> = Sender<'ch, NoopRawMutex, Subscribtion, 4>;
-// type SubscribtionReceiver<'ch> = Receiver<'ch, NoopRawMutex, Subscribtion, 4>;
 
 type Mutex<T> = embassy_sync::mutex::Mutex<NoopRawMutex, T>;
 
@@ -60,7 +48,8 @@ async fn main(_spawner: Spawner) -> ! {
     )
     .unwrap();
 
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    #[cfg(not(feature = "fake"))]
+    let io = hal::IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
     // Async requires the GPIO interrupt to wake futures
     hal::interrupt::enable(
@@ -77,29 +66,32 @@ async fn main(_spawner: Spawner) -> ! {
     let frame_channel = FrameChannel::new();
     let update_channel = FrameChannel::new();
 
-    // let mut buf = [0u8; de1::serial::MAX_ENCODED_LENGTH];
-    let config = uart::config::Config {
-        baudrate: 115200,
-        ..Default::default()
+    #[cfg(not(feature = "fake"))]
+    let (uart_tx, uart_rx) = {
+        let config = hal::uart::config::Config {
+            baudrate: 115200,
+            ..Default::default()
+        };
+        let pins = hal::uart::AllPins::new(
+            io.pins.gpio21.into_push_pull_output(),
+            io.pins.gpio20.into_pull_up_input(),
+            io.pins.gpio7.into_floating_input(),
+            io.pins.gpio8.into_push_pull_output(),
+        );
+        let uart0 = hal::Uart::new_with_config(peripherals.UART0, config, Some(pins), &clocks);
+        uart0.split()
     };
-    let pins = TxRxPins::new_tx_rx(
-        io.pins.gpio21.into_push_pull_output(),
-        io.pins.gpio20.into_pull_up_input(),
-    );
-    // let pins = AllPins::new(
-    //     io.pins.gpio21.into_push_pull_output(),
-    //     io.pins.gpio20.into_pull_up_input(),
-    //     io.pins.gpio7.into_floating_input(),
-    //     io.pins.gpio8.into_push_pull_output(),
-    // );
 
-    let uart0 = Uart::new_with_config(peripherals.UART0, config, Some(pins), &clocks);
-    let (uart_tx, uart_rx) = uart0.split();
-    // let mut pipe1 = Pipe::new();
-    // let (fake_rx, uart_tx) = pipe1.split();
-    // let mut pipe2 = Pipe::new();
-    // let (uart_rx, fake_tx) = pipe2.split();
-    // let mut de1 = De1::new(fake_rx, fake_tx);
+    #[cfg(feature = "fake")]
+    let mut pipe1 = embassy_sync::pipe::Pipe::new();
+    #[cfg(feature = "fake")]
+    let mut pipe2 = embassy_sync::pipe::Pipe::new();
+    #[cfg(feature = "fake")]
+    let (mut de1, uart_tx, uart_rx) = {
+        let (fake_rx, uart_tx) = pipe1.split();
+        let (uart_rx, fake_tx) = pipe2.split();
+        (de1::fake::De1::new(fake_rx, fake_tx), uart_tx, uart_rx)
+    };
 
     let mut serial = serial::SerialInterface::new(
         uart_rx,
@@ -108,19 +100,22 @@ async fn main(_spawner: Spawner) -> ! {
         update_channel.sender(),
     );
 
-    // embassy_futures::join::join3(
-    //     ble_task(
-    //         bluetooth,
-    //         &init,
-    //         frame_channel.sender(),
-    //         update_channel.receiver(),
-    //     ),
-    //     serial.run(),
-    //     de1.run(),
-    // )
-    // .await;
-
     info!("Initialized: running tasks");
+
+    #[cfg(feature = "fake")]
+    embassy_futures::join::join3(
+        ble_task(
+            bluetooth,
+            &init,
+            frame_channel.sender(),
+            update_channel.receiver(),
+        ),
+        serial.run(),
+        de1.run(),
+    )
+    .await;
+
+    #[cfg(not(feature = "fake"))]
     embassy_futures::join::join(
         ble_task(
             bluetooth,
